3092b09c90f5354a39adbb2f97446fa780770649
ibk-code <ibukunoluwarotimi123@gmaiol.com>
2020-08-11 13:34:24 +0100

    clear master  branch

--- src/components/Pages/components/Logo/Logo.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default () => (
-    <img className="logo" src="https://res.cloudinary.com/emekamykael45/image/upload/v1581642175/econnect/logo_ur9lew.png" alt="Logo" />
-);
