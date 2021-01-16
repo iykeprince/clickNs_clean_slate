9085b6fa504d3278a90bc47821dc1b27ccbd3ed8
Zonnaslyric <echezonnachukwu@gmail.com>
2021-01-15 21:04:04 -0800

    update

--- src/components/Account/AddressBookUI.jsx
+++ src/components/Account/AddressBookUI.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect}  from "react";
+import React,{useState}  from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import LargeButton from "../Button/LargeButton";
 import EditAddressComp from "./EditAddressComp";
