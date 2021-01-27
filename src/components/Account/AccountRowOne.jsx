23a287a47d7cce5beb9e39c905978e12abd36d90
Zonnaslyric <echezonnachukwu@gmail.com>
2021-01-26 19:42:45 -0800

    update

--- src/components/Account/AccountRowOne.jsx
+++ src/components/Account/AccountRowOne.jsx
@@ -1,17 +1,4 @@
 import React from "react";
-// import AddressBookUI from "./AddressBookUI";
-// import ChangePasswordUI from "./ChangePasswordUI";
-// import DetailsUI from "./DetailsUI";
-// import MyAccountUI from "./MyAccountUI";
-// import MyOrdersUI from "./MyOrdersUI";
-// import MyPendReviewUI from "./MyPendReviewUI";
-// import MySavedItemsUI from "./MySavedItemsUI";
-// import MyVoucherUI from "./MyVoucherUI";
-// import NewsletterUI from "./NewsletterUI";
-// import IconStore from "../../store/IconStore";
-// import { Route, NavLink, HashRouter } from "react-router-dom";
-// import EditAddressComp from "./EditAddressComp";
-// import * as FaIcon from "react-icons/fa";
 import useWindowDimensions from "../../Hooks/UseWindowDimension";
 import AccountRowOneNavLink from "./AccountRowOneNavLink";
 import AccountRowOneLink from "./AccountRowOneLink";
--- src/components/Account/AccountRowOneLink.jsx
+++ src/components/Account/AccountRowOneLink.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
-// import AddressBookUI from "./AddressBookUI";
-// import ChangePasswordUI from "./ChangePasswordUI";
-// import DetailsUI from "./DetailsUI";
-// import MyAccountUI from "./MyAccountUI";
-// import MyOrdersUI from "./MyOrdersUI";
-// import MyPendReviewUI from "./MyPendReviewUI";
-// import MySavedItemsUI from "./MySavedItemsUI";
-// import MyVoucherUI from "./MyVoucherUI";
-// import NewsletterUI from "./NewsletterUI";
 import IconStore from "../../store/IconStore";
 import { Link} from "react-router-dom";
-// import EditAddressComp from "./EditAddressComp";
 import * as FaIcon from "react-icons/fa";
 
 export default function AccountRowOneLink() {
