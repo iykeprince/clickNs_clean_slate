3331a590f13934ac31e227c7be646f154b8caae2
Zonnaslyric <echezonnachukwu@gmail.com>
2021-03-20 02:02:45 -0700

    some functionalities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./assets/sass/main.scss";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ProductDetail from "./pages/ProductDetail";
 import PhonenTablet from "./pages/Categry/PhonenTablet";
 import Supermarket from "./pages/Categry/Supermarket";
